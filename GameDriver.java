1dc4ae13020dafe096787bb89fb77c2e45058757
Zay <mobmine7@gmail.com>
2026-06-04 10:35:37 -0400

    Added a bit more to options of the boss fihgt, may remake the sprites and add some more tiles and possibly rooms

--- GameDriver.java
+++ GameDriver.java
@@ -15,8 +15,6 @@ public class GameDriver
 {
     public static void main(String[] args) throws IOException
     {
-        //Redirect Java to this folder
-        //System.setProperty("user.dir", new File(".").getAbsolutePath()); 
 
         //Elements
         AudioPlayer audioPlayer = new AudioPlayer();
